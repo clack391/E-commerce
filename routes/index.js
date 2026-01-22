f5e507b46e199a6fa5c08999314dad57b67ffbb5
Precious Onotu <clack391@gmail.com>
2026-01-22 23:22:44 +0100

    swagger fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-const express = require('express');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello World! The E-commerce API is running.');
+});
 
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
